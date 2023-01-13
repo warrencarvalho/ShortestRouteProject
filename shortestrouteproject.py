b16509cc407a1ec99e957ed6f7e32f6cd7c7f145
Freezanator <freezanator95@yahoo.com>
2023-01-13 10:09:27 +0800

    Update shortestrouteproject.py

--- shortestrouteproject.py
+++ shortestrouteproject.py
@@ -11,6 +11,7 @@ def dijkstra(graph, start, goal):
     distances = {start: 0}
 
     while queue:
+
         current_distance, current_node = heapq.heappop(queue)
         if current_node in visited:
             continue
@@ -76,10 +77,6 @@ def calculations(destination):
         "K": "Hospital Az-Zahra"
     }
 
-    distanceSerdang = str1[0]
-    distanceKajang = str2[0]
-    distanceZahra = str3[0]
-
     # Strips the directions output from Dijkstra's algorithm
     str4 = str1[1]
     str5 = str2[1]
@@ -108,10 +105,38 @@ def calculations(destination):
     print(resultB)
     print(resultC)
 
-    # Places results of every hospital in an array
-    results = [resultA, resultB, resultC]
+    distanceSerdang = str1[0]
+    distanceKajang = str2[0]
+    distanceZahra = str3[0]
+
+    distanceArray = [distanceSerdang, distanceKajang, distanceZahra]
+    directionArray = [resultA, resultB, resultC]
+
+    # Sorts directionArray
+    if (distanceArray[0] >= distanceArray[1]):
+
+        distanceArray[0], distanceArray[1] = distanceArray[1], distanceArray[0]
+        directionArray[0], directionArray[1] = directionArray[1], directionArray[0]
+
+    if (distanceArray[0] >= distanceArray[2]):
+
+        distanceArray[0], distanceArray[2] = distanceArray[2], distanceArray[0]
+        directionArray[0], directionArray[2] = directionArray[2], directionArray[0]
+
+    if (distanceArray[1] >= distanceArray[2]):
+
+        distanceArray[1], distanceArray[2] = distanceArray[2], distanceArray[1]
+        directionArray[1], directionArray[2] = directionArray[2], directionArray[1]
 
     # Returns results
+    results = []
+    for l in range(0, 3):
+
+        results.append(directionArray[l])
+        results.append("Estimated time: " + str(distanceArray[l]) + " minutes")
+        results.append("\n")
+
+    print(results)
     return '\n'.join(str(x) for x in results)
 
 # GUI
@@ -122,36 +147,40 @@ layout1 = [
 
     # Labelled map
     [
-        sg.Image('resources/finalmap.png')
-    ],
+        sg.Image('resources/finalmap.png'),
 
-    # Radio buttons
-    [
-        sg.Radio('B11', "RADIO1", default=False),
-        sg.Radio('903', "RADIO1"),
-        sg.Radio('Persiaran Jaya', "RADIO1"),
-        sg.Radio('B50', "RADIO1"),
-        sg.Radio('E18', "RADIO1"),
-        sg.Radio('E7', "RADIO1"),
-        sg.Radio('Semenyih', "RADIO1"),
-        sg.Radio('Seksyen 8', "RADIO1")
-    ],
+        # Select site of incident
+        [
+            sg.Text('Select site of incident')
+        ],
 
-    # OK and Close buttons
-    [
-        sg.OK(), sg.Button('Close')
-    ],
+        # Radio buttons
+        [
+            sg.Radio('B11', "RADIO1", default=False),
+            sg.Radio('903', "RADIO1"),
+            sg.Radio('Persiaran Jaya', "RADIO1"),
+            sg.Radio('B50', "RADIO1"),
+            sg.Radio('E18', "RADIO1"),
+            sg.Radio('E7', "RADIO1"),
+            sg.Radio('Semenyih', "RADIO1"),
+            sg.Radio('Seksyen 8', "RADIO1")
+        ],
 
-    # FASTEST ROUTE text
-    [
-        sg.Text("FASTEST ROUTE", visible=False, key='fastest')
-    ],
+        # FASTEST ROUTE text
+        [
+            sg.Text("FASTEST ROUTE", visible=False, key='fastest')
+        ],
 
-    # Results (initially is invisible before inputs are sent for calculation)
-    [
-        sg.Text(calculations("B"), visible=False, key='results')
+        # Results (initially is invisible before inputs are sent for calculation)
+        [
+            sg.Text(calculations("B"), visible=False, key='results')
+        ],
+
+            # Calculate and Close buttons
+        [
+            sg.Button('Calculate'), sg.Button('Close')
+        ]
     ]
-
 ]
 
 # Create the Window
@@ -167,41 +196,41 @@ while True:
         break
 
     # If OK button is clicked
-    if event == "OK":
+    if event == 'Calculate':
         print(event)
-
+        print(values)
         # Changes results to be visible after calculations
         window['fastest'].update(visible=True)
 
-        if values[1] == True:
+        if values[0] == True:
 
             window['results'].update(calculations("B"), visible=True)
 
-        if values[2] == True:
+        if values[1] == True:
 
             window['results'].update(calculations("C"), visible=True)
 
-        if values[3] == True:
+        if values[2] == True:
 
             window['results'].update(calculations("D"), visible=True)
 
-        if values[4] == True:
+        if values[3] == True:
 
             window['results'].update(calculations("E"), visible=True)
 
-        if values[5] == True:
+        if values[4] == True:
 
             window['results'].update(calculations("F"), visible=True)
 
-        if values[6] == True:
+        if values[5] == True:
 
             window['results'].update(calculations("G"), visible=True)
 
-        if values[7] == True:
+        if values[6] == True:
 
             window['results'].update(calculations("I"), visible=True)
 
-        if values[8] == True:
+        if values[7] == True:
 
             window['results'].update(calculations("J"), visible=True)
 
